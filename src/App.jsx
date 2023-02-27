5210a7a46dd3b90879aad642a95efaccd2017c91
herom <jerome.manset22@gmail.com>
2023-02-28 00:51:02 +0800

    Add elements to the Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Attribution from './Attribution'
+import Attribution from './components/Attribution'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      App
+      <Navbar />
       <Attribution />
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <img src="./src/assets/images/logo.svg" alt="logo" />
+      <img src="./src/assets/images/icon-hamburger.svg" alt="hamburger menu" />
+      <ul>
+        <li>Abount</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   )
 }
 
