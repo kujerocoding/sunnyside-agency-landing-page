62f2ceeabeef40c69a81b1c85afdc3e00771ca06
herom <jerome.manset22@gmail.com>
2023-02-28 00:27:47 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Attribution from './Attribution'
+
+const App = () => {
+  return (
+    <div>
+      App
+      <Attribution />
+    </div>
+  )
+}
+
+export default App
