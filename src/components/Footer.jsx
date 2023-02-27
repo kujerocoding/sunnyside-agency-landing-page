940748d64b329426ce3b87246c9e334e8869671b
herom <jerome.manset22@gmail.com>
2023-02-28 01:26:22 +0800

    Add elements to the Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div>
+        <img src="src/assets/images/mobile/image-gallery-milkbottles.jpg" alt="white bottle" />
+        <img src="src/assets/images/mobile/image-gallery-orange.jpg" alt="orange" />
+        <img src="src/assets/images/mobile/image-gallery-cone.jpg" alt="ice cream cone" />
+        <img src="src/assets/images/mobile/image-gallery-sugar-cubes.jpg" alt="sugar cubes" />
+      </div>
+      <h2>sunnyside</h2>
+      <ul>
+        <li>About</li>
+        <li>Servies</li>
+        <li>Projects</li>
+      </ul>
+      <div>
+        <img src="src/assets/images/icon-facebook.svg" alt="facebook" />
+        <img src="src/assets/images/icon-instagram.svg" alt="instagram" />
+        <img src="src/assets/images/icon-twitter.svg" alt="twitter" />
+        <img src="src/assets/images/icon-pinterest.svg" alt="pinterest" />
+      </div>
+    </footer>
   )
 }
 
