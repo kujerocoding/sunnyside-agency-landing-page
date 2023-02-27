7539567ffe2da65b42b6e937b87f9052231374ea
herom <jerome.manset22@gmail.com>
2023-02-28 05:24:32 +0800

    Add styles to the upper Main component elements

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,29 +5,32 @@ const Main = () => {
     <main>
         <header>
             <div className='img--orange--container'>
-            <span className='header__img--orange' role="img" aria-label='orange' ></span>
+                <span className='header__img--orange' role="img" aria-label='orange' ></span>
             </div>
-           
-                <div className='header--info--container'>
-                  <p>We are creatives</p>
-                  <img className="header__img--arrow" src="../src/assets/images/icon-arrow-down.svg" alt="arrow down" />
-                 </div>
+            <div className='header--info--container'>
+                <p>We are creatives</p>
+                <img className="header__img--arrow" src="../src/assets/images/icon-arrow-down.svg" alt="arrow down" />
+            </div>
         </header>
         <div>
             <span className='img--egg' role="img" aria-label='egg'></span>
-            <h1>Transform your brand</h1>
-             <p>
-                We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.
-            </p>
-            <p>Learn more</p>
+            <div className='main--text--container'>
+                <h1>Transform your brand</h1>
+                <p className='main--text--info'>
+                    We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.
+                </p>
+                <p className='main--text--learnmore learn--yellow'>Learn more</p>
+            </div>
         </div>
         <div>
-            <span role="img" aria-label='cup'></span>
-            <h1>Stand out to the right audience</h1>
-             <p>
-             Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.
-            </p>
-            <p>Learn more</p>
+            <span className='img--cup' role="img" aria-label='cup'></span>
+            <div className='main--text--container'>
+                <h1>Stand out to the right audience</h1>
+                <p className='main--text--info'>
+                Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.
+                </p>
+                <p className='main--text--learnmore learn--softred'>Learn more</p>
+            </div>
         </div>
         <div>
             <span role="img" aria-label='berry'>
