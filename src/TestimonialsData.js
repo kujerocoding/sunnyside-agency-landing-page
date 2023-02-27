7d4ec138eff4ff3bd93cdbc02f7dcfc6b3b6bc55
herom <jerome.manset22@gmail.com>
2023-02-28 06:05:48 +0800

    Put the testimonials data into js object file

---
+++ src/TestimonialsData.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        imgURL: 'image-emily.jpg',
+        testimony: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+        name: 'Emily R.',
+        jobPosition: 'Marketing Director'
+    },
+    {
+        imgURL: 'image-thomas.jpg',
+        testimony: " Sunnyside's enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
+        name: 'Thomas S.',
+        jobPosition: 'Chief Operating Officer'
+    },
+    {
+        imgURL: 'image-jennie.jpg',
+        testimony: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+        name: 'Jennie F.',
+        jobPosition: 'Business Owner'
+    },
+]
