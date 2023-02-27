905fbc175624b99ca1137cdab728116b65102882
herom <jerome.manset22@gmail.com>
2023-02-28 01:11:21 +0800

    Add elements to the Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Attribution from './components/Attribution'
 import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Main />
       <Attribution />
     </div>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>
-      main
-    </div>
+    <main>
+        <div>
+            <img src="../src/assets/images/mobile/image-header.jpg" alt="orange fruit" />
+        </div>
+        <div>
+            <span role="img" aria-label='egg'></span>
+            <h1>We are creatives Transform your brand</h1>
+             <p>
+                We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.
+            </p>
+            <p>Learn more</p>
+        </div>
+        <div>
+            <span role="img" aria-label='cup'></span>
+            <h1>Stand out to the right audience</h1>
+             <p>
+             Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.
+            </p>
+            <p>Learn more</p>
+        </div>
+        <div>
+            <span role="img" aria-label='berry'>
+                <h1>Graphic Design</h1>
+                <p>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients' attention.</p>
+            </span>
+        </div>
+        <div>
+            <span role="img" aria-label='orange'>
+                <h1>Photography</h1>
+                <p>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
+            </span>
+        </div>
+    </main>
   )
 }
 
