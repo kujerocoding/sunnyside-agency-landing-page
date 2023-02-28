02e6670db2f3e53ca9b7ed9697a3411429d66a7f
herom <jerome.manset22@gmail.com>
2023-03-01 00:23:00 +0800

    Add media query. Update nav style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
 const Navbar = (props) => {
+
+  const navClassList = props.isMenuOpen ? 'nav--active' : 'nav--lists'
+
   return (
     <nav>
       <div className='nav--icons--container'>
         <img src="./src/assets/images/logo.svg" alt="logo" />
-        <img src="./src/assets/images/icon-hamburger.svg" 
+        <img className='icon--hamburger' src="./src/assets/images/icon-hamburger.svg" 
         alt="hamburger menu" onClick={props.toggleMenu}/>
       </div>
       
-      {props.isMenuOpen && 
-        <ul>
+      
+        <ul className={navClassList}>
           <li>About</li>
           <li>Services</li>
           <li>Projects</li>
           <li>Contact</li>
-      </ul>}
+      </ul>
     </nav>
   )
 }
